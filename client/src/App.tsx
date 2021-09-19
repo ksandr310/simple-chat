ac58afdee703ea1eb2f7c4d0e4495addf97d2320
ksandr310 <ksandr0310@gmail.com>
2021-09-19 20:07:33 +0300

    added react-router and authPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthPage } from './pages';
 
 import styles from './App.module.scss';
 
 const CLASS = 'app';
 
-const App: React.FC = () => {
-    return (
-        <div className={styles[CLASS]}>
-            <AuthPage />
-        </div>
-    );
-};
+const App: React.FC = () => (
+    <div className={styles[CLASS]}>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<AuthPage />} />
+                <Route path={'/chat/:name'} element={<div>{'chat page'}</div>} />
+            </Routes>
+        </BrowserRouter>
+    </div>
+);
 
 export default App;
--- client/src/pages/AuthPage/AuthPage.tsx
+++ client/src/pages/AuthPage/AuthPage.tsx
@@ -1,8 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './AuthPage.module.scss';
 
 const CLASS = 'auth-page';
 
+const title = 'Hey there';
+const description = 'What should your peers call you?';
+const inputPlaceholder = 'You name or nickname';
+const startChat = 'Start chatting';
+
 export const AuthPage: React.FC = () => {
-    return <div className={styles[CLASS]}></div>;
+    const navigate = useNavigate();
+    const [name, setName] = useState('');
+
+    const navigateToChatPage = () => {
+        if (name !== '') navigate?.(`/chat/${name}`);
+    };
+
+    return (
+        <div className={styles[CLASS]}>
+            <div className={styles[`${CLASS}__title`]}>{title}</div>
+            <div className={styles[`${CLASS}__description`]}>{description}</div>
+            <div className={styles[`${CLASS}__input`]}>
+                <input
+                    onKeyPress={({ key }) => {
+                        if (key === 'Enter') navigateToChatPage();
+                    }}
+                    type={'text'}
+                    autoComplete={'name'}
+                    placeholder={inputPlaceholder}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className={styles[`${CLASS}__button`]}>
+                <button disabled={!name} onClick={navigateToChatPage}>
+                    {startChat}
+                </button>
+            </div>
+        </div>
+    );
 };
